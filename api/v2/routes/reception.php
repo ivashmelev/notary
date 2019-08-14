a72ca2d0144ecfc72989a193d68531c18cb8e5ec
Шмелев Иван <ishmelev@1ditis.ru>
2019-08-14 16:19:44 +0300

    api

---
+++ api/v2/routes/reception.php
@@ -0,0 +1,35 @@
+<?php
+// Запись на прием и получить список заявок
+header('Access-Control-Allow-Origin: *');
+
+require_once ('../../../modules/sql/config.php');
+require_once ('../../../modules/sql/auth.php');
+
+$connect = mysqli_connect($HOST, $USERNAME, $PASSWORD ,$DATABASE);
+
+if (!$connect) {
+  die("Ошибка: Невозможно установить соединение с MySQL.");
+} else {
+
+  switch($_SERVER['REQUEST_METHOD']){
+    case 'POST':
+      if(isset($_POST['date']) && isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['mail'])){
+        $result = mysqli_query($connect, 'CALL func_api_v1_post_reception(
+          '.$_POST['date'].', '.$_POST['name'].', '.$_POST['phone'].', '.$_POST['mail'].')');
+      }
+    break;
+    case 'GET':
+      auth($connect);
+      $result = mysqli_query($connect, 'CALL func_api_v1_get_reception()');
+  }
+
+  if($result == ''){
+    die('403 Bad Request');
+  }
+
+  $data = json_encode(pg_fetch_all($result));
+  echo $data;
+}
+
+?>
+
