2caef3935abbb709dc9d2ea1225d2a2c36c6e115
serdubov <s.dubov3.14@gmail.com>
2019-08-16 16:13:05 +0300

    biography

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -7,6 +7,8 @@ import styled from 'styled-components'
 import responsive from '../responsive'
 import openCite from '../assets/img/openCite.svg';
 import closeCite from '../assets/img/closeCite.svg';
+import personImg from '../assets/img/person.png';
+
 
 
 export default class About extends Component {
@@ -33,7 +35,19 @@ export default class About extends Component {
               </AboutCiteAuthor>
             </AboutContentColumn>
           </AboutContentText>
-          
+          <AboutPersonWrapper>
+            <AboutPersonPortret>
+              <AboutPersonPortretBack />
+              <AboutPersonPortretImg src={personImg} />
+            </AboutPersonPortret>
+            <AboutPersonBiography>
+              <PersonBiographyTitle>Кондратьева Екатерина Алексеевна</PersonBiographyTitle>
+              <PersonBiographySubTitle>Юрист</PersonBiographySubTitle>
+              <PersonBiographyDescription>
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+              </PersonBiographyDescription>
+            </AboutPersonBiography>
+          </AboutPersonWrapper>
         </AboutContent>
         <Navigation title='УСЛУГИ' subtitle='СЛЕДУЮЩИЙ РАЗДЕЛ' />
       </AboutWrapper>
@@ -41,6 +55,95 @@ export default class About extends Component {
   }
 }
 
+const PersonBiographyDescription = styled.div`
+  font-family: Montserrat Regular;
+  font-size: 18px;
+  line-height: 34px;
+  letter-spacing: 0.05em;
+  color: #2D2D2D;
+  @media ${responsive.tablet} {
+    font-size: 16px;
+    line-height: 25px;
+    letter-spacing: 0em;
+  }
+`
+const PersonBiographySubTitle = styled.div`
+  font-family: Montserrat Bold;
+  font-size: 16px;
+  line-height: 34px;
+  letter-spacing: 0.05em;
+  color: #2D2D2D;
+  margin: 0 0 20px;
+  @media ${responsive.tablet} {
+    font-size: 16px;
+    line-height: 25px;
+  }
+`
+const PersonBiographyTitle = styled.div`
+  font-family: Montserrat Bold;
+  font-size: 20px;
+  line-height: 34px;
+  letter-spacing: 0.05em;
+  color: #E6B980;
+  margin: 0 0 30px;
+  @media ${responsive.tablet} {
+    font-size: 20px;
+    line-height: 25px;
+    margin: 0 0 20px;
+  }
+`
+const AboutPersonBiography = styled.div`
+  width: 500px;
+  padding: 35px 0;
+  @media ${responsive.tablet} {
+    max-width: 500px;
+    width: 100%;
+    padding: 20px 0;
+  }
+`
+const AboutPersonPortretImg = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-image: url(${props => props.src});
+  background-position: center;
+  background-size: cover;
+`
+const AboutPersonPortretBack = styled.div`
+  width: 368px;
+  height: 526px;
+  background: #2D2D2D;
+  position: absolute;
+  top: 0; left: 0;
+  @media ${responsive.tablet} {
+    display: none;
+  }
+`
+const AboutPersonPortret = styled.div`
+  width: 368px;
+  height: 526px;
+  padding: 35px;
+  position: relative;
+  @media ${responsive.tablet} {
+    max-width: 345px;
+    width: 100%;
+    height: 400px;
+    padding: 0;
+  }
+`
+const AboutPersonWrapper = styled.div`
+  width: 100%;
+  padding: 200px 0 0 0;
+  display: flex;
+  justify-content: space-between;
+  @media ${responsive.tabletB} {
+    flex-direction: column;
+    align-items: center;
+  }
+  @media ${responsive.tablet} {
+    padding: 140px 0 0 0;
+  }
+`
 const AboutWrapper = styled.div``;
 
 const AboutContent = styled.div`
