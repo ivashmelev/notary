7a01779930cbb2b8a03e944fa4a537c6a864a3db
Шмелев Иван <ishmelev@1ditis.ru>
2019-08-16 14:14:26 +0300

    Service

--- admin-panel/src/components/Element/index.js
+++ admin-panel/src/components/Element/index.js
@@ -40,16 +40,15 @@ export default class Element extends Component {
 
       // this.sendRequest(event.target.id, event.target.value, 'description');
       document.removeEventListener('keydown', this.onKeyPress);
-      this.props.onHandleActiveButton(this.props.active);
     }
   }
 
   render() {
-    const { event, text, id, view, name, active } = this.props;
+    const { event, text, id, view, name } = this.props;
     return (
       <ElementWrapper >
         {this.state.view === 'text' ?
-          <ElementText className='wall-input' data-name={name} onClick={() => { this.props.onHandleActiveButton(active); this.setState({ view: this.props.view }) }}>{this.state.value}</ElementText> :
+          <ElementText className='wall-input' data-name={name} onClick={() => this.setState({ view: this.props.view })}>{this.state.value}</ElementText> :
           this.state.view === 'input' ?
             <ElementInput id={id} onKeyPress={this.onKeyPress} type='text' placeholder={this.state.value} onChange={e => this.handleTextChange({ value: e.target.value })} value={this.state.value} /> :
             this.state.view === 'textarea' ?
