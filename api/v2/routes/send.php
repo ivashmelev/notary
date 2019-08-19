208ede6dbc3e84dccfae9f059403b17db2c5f3db
Шмелев Иван <ishmelev@1ditis.ru>
2019-08-19 22:04:02 +0300

    send

--- api/v2/routes/send.php
+++ api/v2/routes/send.php
@@ -1,7 +1,7 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 
-require_once ('../../modules/mail.php');
+require_once ('../../../modules/mail.php');
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
@@ -9,5 +9,5 @@ $adminMail = $_POST['admin_mail'];
 $mail = $_POST['mail'];
 $comment = $_POST['comment'];
 
-smtpmail("Заявка на сайте от $name", "$adminMail", "Заявка на сайте от $name", "Имя - $name </br> Телефон - $phone </br> Почта - $mail </br> Комментарий - $comment </br>");
+smtpmail("Заявка на сайте от $name", "$adminMail", "Заявка на сайте от $name", "Имя - $name <br> Телефон - $phone <br> Почта - $mail <br> Комментарий - $comment <br>");
 ?>
